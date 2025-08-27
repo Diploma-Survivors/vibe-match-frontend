3ae7753ccde779b642d67ec723b2fccedca92dc3
html <22120436@student.hcmus.edu.vn>
2025-08-28 00:04:30 +0700

    extract the component and  update the css for the problem detail page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Footer, Header } from "@/components/layout";
 import { ThemeProvider } from "@/components/providers";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import LayoutWrapper from "@/components/layout/layout-wrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,13 +19,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <Header />
-          <main className="pt-16">{children}</main>
-          <Footer />
+          <LayoutWrapper>{children}</LayoutWrapper>
         </ThemeProvider>
       </body>
     </html>
--- src/components/problem/index.ts
+++ src/components/problem/index.ts
@@ -4,3 +4,5 @@ export { default as ProblemSidebar } from "./problem-sidebar";
 export { default as ProblemStats } from "./problem-stats";
 export { default as ProblemStatsNew } from "./problem-stats-new";
 export { default as ProblemTable } from "./problem-table";
+export { default as ProblemDetail } from "./problem-detail-tab";
+export { default as ProblemSubmissions } from "./problem-submissions-tab";
